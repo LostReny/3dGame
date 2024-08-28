bda916ab84684ce21ff9e11198c5f5f251d9e443
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2024-08-28 12:20:33 -0300

    update statemachine

--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/LostWorlds/State Machine/StateMachine.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/LostWorlds/State Machine/StateMachine.cs
@@ -5,69 +5,45 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 using NaughtyAttributes;
 
-public class StateMachine : MonoBehaviour
+public class StateMachine<T> where T : System.Enum
 {
-    public enum States{
-        NONE,
-    }
-
-    public Dictionary<States, StateBase> DictionaryState;
+    
+    public Dictionary<T, StateBase> dictionaryState;
     private StateBase _currentState;
     public float timeToStartGame = 1f;
 
-    public static StateMachine Instance;
+
+   public void Init()
+   {
+      dictionaryState = new Dictionary<T, StateBase>();
+   }
 
 
-    private void Awake() {
-
-      Instance = this;
-
-        DictionaryState = new Dictionary<States, StateBase>();
-        DictionaryState.Add(States.NONE, new StateBase());
-        SwitchStates(States.NONE);
-    }
-
-    [Button]
-    private void StartGame(){
-        SwitchStates(States.NONE);
-    }
-
-#if UNITY_EDITOR
-    #region DEBUG
-    [Button]
-    private void ChangeStateToStateX()
+    public void RegisterStates(T typeEnum, StateBase state) 
     {
-        SwitchStates(States.NONE);
-    }
-    #endregion
-#endif
 
-    public void SwitchStates(States state)
+      dictionaryState.Add(typeEnum, state);
+       
+    }
+    
+    public void SwitchStates(T state)
    {
       if (_currentState != null)
       {
          _currentState.OnStateExit();
       }
 
-      _currentState = DictionaryState[state];
+      _currentState = dictionaryState[state];
       if(_currentState != null){ _currentState.OnStateEnter();}
    }
 
-   private void Update()
+   public void Update()
    {
       if (_currentState != null)
       {
          _currentState.OnStateStay();
       }
-
-      if (Input.GetKeyDown(KeyCode.Q)) 
-      {
-         SwitchStates(States.NONE);
-      }
    }
 
-   public void ResetPosition(){
-      SwitchStates(States.NONE);
-   }
 
 }
