62624caa591c1b6332bb20ec4973eca7ace18483
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-10-12 19:49:13 -0300

    Player controller

--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/LostWorlds/State Machine/StateMachine.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/LostWorlds/State Machine/StateMachine.cs
@@ -5,66 +5,67 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 using NaughtyAttributes;
 
-namespace LostWordls.StateMachine {
-
-public class StateMachine<T> where T : System.Enum
+namespace LostWordls.StateMachine
 {
-    
-    public Dictionary<T, StateBase> dictionaryState;
-    private StateBase _currentState;
-    public float timeToStartGame = 1f;
 
-    public StateBase CurrentState
+    public class StateMachine<T> where T : System.Enum
     {
-      get{ return _currentState;}
+
+        public Dictionary<T, StateBase> dictionaryState;
+        private StateBase _currentState;
+        public float timeToStartGame = 1f;
+
+        public StateBase CurrentState
+        {
+            get { return _currentState; }
+        }
+
+        public void Init()
+        {
+            dictionaryState = new Dictionary<T, StateBase>();
+        }
+
+        public void RegisterStates(T typeEnum, StateBase state)
+        {
+            if (!dictionaryState.ContainsKey(typeEnum))
+            {
+                dictionaryState.Add(typeEnum, state);
+                Debug.Log($"State {typeEnum} registered successfully.");
+            }
+        }
+
+
+        public void SwitchStates(T state)
+        {
+            if (dictionaryState.ContainsKey(state))
+            {
+                if (_currentState != null)
+                {
+                    _currentState.OnStateExit();
+                }
+
+                _currentState = dictionaryState[state];
+                if (_currentState != null)
+                {
+                    _currentState.OnStateEnter();
+                }
+                Debug.Log($"Switched to state: {state}");
+            }
+            else
+            {
+                Debug.LogError($"State {state} not found in dictionary.");
+            }
+        }
+
+
+        public void Update()
+        {
+            if (_currentState != null)
+            {
+                _currentState.OnStateStay();
+            }
+        }
+
     }
-
-   public void Init()
-   {
-      dictionaryState = new Dictionary<T, StateBase>();
-   }
-
-   public void RegisterStates(T typeEnum, StateBase state) 
-   {
-      if(!dictionaryState.ContainsKey(typeEnum))
-      {
-         dictionaryState.Add(typeEnum, state);
-         Debug.Log($"State {typeEnum} registered successfully.");
-      }
-   }
-
-    
-      public void SwitchStates(T state)
-   {
-      if (dictionaryState.ContainsKey(state))
-      {
-         if (_currentState != null)
-         {
-               _currentState.OnStateExit();
-         }
-
-         _currentState = dictionaryState[state];
-         if (_currentState != null)
-         {
-               _currentState.OnStateEnter();
-         }
-         Debug.Log($"Switched to state: {state}");
-      }
-      else
-      {
-         Debug.LogError($"State {state} not found in dictionary.");
-      }
-   }
-
-
-   public void Update()
-   {
-      if (_currentState != null)
-      {
-         _currentState.OnStateStay();
-      }
-   }
-
-}
 
 }
--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/Player/PlayerController.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Player/PlayerController.cs
@@ -4,93 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
-    public float runSpeed;
-    public float jumpForce = 7f;
-    private float initialSpeed; 
+    public Animator animator; 
+    
+    public CharacterController characterController;
+    public float speed = 1.0f;
+    public float turnSpeed = 1.0f;
+    public float gravity = -9f;
 
-    private Rigidbody rb;
-    private bool isGrounded = true;
+    private float vSpeed = 0f;
 
-    private Vector3 direction;
-    private bool isRunning;
-    private bool isJumping;
-
-    void Start()
+    private void Update()
     {
-        rb = GetComponent<Rigidbody>();
-
-        initialSpeed = speed;
-        
+        OnMove();
     }
 
-    void Update()
+    public void OnMove()
     {
-        Inputs(); 
-    }
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
 
-    public void Inputs()
-    {
-       
-        direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        var inputAxisVertical = Input.GetAxis("Vertical");
+        var speedVector = transform.forward * inputAxisVertical * speed;
 
-        
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        vSpeed = gravity * Time.deltaTime;
+        speedVector.y = vSpeed;
+
+        characterController.Move(speedVector * Time.deltaTime);
+
+        if(inputAxisVertical != 0)
         {
-            isJumping = true;
+            animator.SetBool("Run", true);
         }
-
-       
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        else
         {
-            Run();
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            StopRunning();
-        }
-    }
-
-    void FixedUpdate()
-    {
-        Walk();
-
-        if (isJumping)
-        {
-            Jump();
-            isJumping = false; 
-        }
-    }
-
-    public void Walk()
-    {
-        Vector3 newPosition = rb.position + direction * speed * Time.fixedDeltaTime; 
-        rb.MovePosition(newPosition);
-    }
-
-    public void Jump()
-    {
-        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse); 
-        isGrounded = false;
-    }
-
-    public void Run()
-    {
-        speed = runSpeed;
-        isRunning = true;
-    }
-
-    public void StopRunning()
-    {
-        speed = initialSpeed; 
-        isRunning = false;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true; 
+            animator.SetBool("Run", false);
         }
     }
 }
