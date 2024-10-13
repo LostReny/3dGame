409ef2e823e577a53308ca38e6aaa265b823120c
Isabelle Reny <isabelleribaldo@hotmail.com>
2024-10-12 23:22:29 -0300

    Player Jump and Run

--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/Player/PlayerController.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Player/PlayerController.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator; 
-    
+    [Header("Animator")]
+    public Animator animator;
+
+    [Header("Movement")]
     public CharacterController characterController;
     public float speed = 1.0f;
     public float turnSpeed = 1.0f;
     public float gravity = -9f;
+
+    [Header("Jump")]
+    public float jumpSpeed = 15f;
+    public KeyCode jumpKeyCode = KeyCode.Space;
+
+    [Header("Run")]
+    public float runSpeed = 1.5f ;
+    public KeyCode runKeyCode = KeyCode.LeftShift;
 
     private float vSpeed = 0f;
 
@@ -18,6 +28,7 @@ public class PlayerController : MonoBehaviour
         OnMove();
     }
 
+    #region movement
     public void OnMove()
     {
         transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
@@ -25,11 +36,30 @@ public class PlayerController : MonoBehaviour
         var inputAxisVertical = Input.GetAxis("Vertical");
         var speedVector = transform.forward * inputAxisVertical * speed;
 
-        vSpeed = gravity * Time.deltaTime;
+        OnJump();
+
+        vSpeed -= gravity * Time.deltaTime;
         speedVector.y = vSpeed;
 
         characterController.Move(speedVector * Time.deltaTime);
 
+        // run e animação
+        var isWalking = inputAxisVertical != 0;
+        if (isWalking)
+        {
+            if (Input.GetKey(runKeyCode)) 
+            { 
+                speedVector *= runSpeed;
+                animator.speed = runSpeed;
+            }
+            else 
+            {
+                animator.speed = 1;
+            }
+        }
+
+
+        // animação
         if(inputAxisVertical != 0)
         {
             animator.SetBool("Run", true);
@@ -38,5 +68,24 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("Run", false);
         }
+
+
     }
+    #endregion
+
+    #region jump
+    public void OnJump()
+    {
+        if (characterController.isGrounded)
+        {
+            vSpeed = 0;
+            if (Input.GetKeyDown(jumpKeyCode))
+            {
+                vSpeed = jumpSpeed;
+            }
+        }
+    }
+    #endregion
+
+   
 }
