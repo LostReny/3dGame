3464e42cf5021606a0d3430bd186e0b0fc4c9b4b
Isabelle Reny <isabelleribaldo@hotmail.com>
2025-01-04 18:49:07 -0300

    Collect Itens

--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Collectibles/ItemCollectBase.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Collectibles/ItemCollectBase.cs
@@ -2,50 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemCollectBase : MonoBehaviour
-{
-    public string compareTag = "Player";
-
-    public float timeToHide = 1;
-    public GameObject graphicItem;
-
-    /*[Header("Sounds")]
-    public AudioSource audioSource;*/
-
-
-    public void OnTriggerEnter(Collider collision) {
-
-        if(collision.transform.CompareTag(compareTag)){
-            Collect();
-        }
-        
-    }
-
-    protected virtual void Collect()
+namespace Itens { 
+    public class ItemCollectBase : MonoBehaviour
     {
-        Destroy(gameObject, 0.5f);
+        public ItemType itemType;
 
-        if (graphicItem != null) graphicItem.SetActive(true);
-        Invoke("HideObject", timeToHide);
-        OnCollect();
-    }
+        public string compareTag = "Player";
 
-    private void HideObject()
-    {
-        gameObject.SetActive(false);
-        
-    }
+        public float timeToHide = 1;
+        public GameObject graphicItem;
 
-    protected virtual void OnCollect()
-    {
+        public Collider _collider;
 
-            /*if (!audioSource.isPlaying) {
-                audioSource.transform.SetParent(null);
-                audioSource.Play();
-                //Debug.Log("Esta tocando");
+        public void OnTriggerEnter(Collider collision) {
+
+            if(collision.transform.CompareTag(compareTag)){
+                Collect();
             }
+        
+        }
 
-            VFXManager.Instance.PlayVFXByType(VFXManager.VFXType.COIN, transform.position);*/
+        public void Start()
+        {
+            _collider = GetComponent<Collider>();
+            _collider.enabled = true;
+        }
+
+        protected virtual void Collect()
+        {
+            //Destroy(gameObject, 0.5f);
+            if (_collider != null) _collider.enabled = false;
+
+            if (graphicItem != null) graphicItem.SetActive(true);
+            Invoke("HideObject", timeToHide);
+            OnCollect();
+        }
+
+        private void HideObject()
+        {
+            gameObject.SetActive(false);
+        
+        }
+
+        protected virtual void OnCollect()
+        {
+            ItemCollectManager.Instance.AddByType(itemType);
+        }
+
     }
-
 }
