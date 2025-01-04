b7bdbf7fa8f9c94e3ae87d2ed57c6ec53990c517
Isabelle Reny <isabelleribaldo@hotmail.com>
2025-01-04 17:40:52 -0300

    itemCollectManager
    Scriptable objects

--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Collectibles/ItemCollectCoin.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Collectibles/ItemCollectCoin.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Itens;
 
 public class ItemCollectCoin : ItemCollectBase
 {
     protected override void OnCollect(){
 
         base.OnCollect();
-        ItemCollectManager.Instance.AddCoins();
+        ItemCollectManager.Instance.AddByType(ItemType.COIN);
+        //collider.enable = false;
 
     }
     
--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Collectibles/ItemCollectManager.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Collectibles/ItemCollectManager.cs
@@ -3,35 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using LostWordls.Singleton;
+using JetBrains.Annotations;
 
-public class ItemCollectManager : Singleton<ItemCollectManager>
+
+namespace Itens
 {
-    public SOInt coins;
-
-    private void Awake() {
-
-        if(Instance == null)
-        Instance = this;
-        else 
-        Destroy(gameObject);
-       
+    public enum ItemType
+    {
+        COIN,
+        LIFE_PACK
     }
 
-    private void Update() {
+    public class ItemCollectManager : Singleton<ItemCollectManager>
+    {
+        public List<ItensSetup> itensSetups;
+
+        private void Awake()
+        {
+
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+
+        }
+
+
+        private void Start()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            foreach(var i in itensSetups)
+            {
+                i.soInt.value = 0;
+            }
+        }
+
+        public void AddByType(ItemType itemType, int amount = 1)
+        {
+            if (amount < 0) return;
+
+            itensSetups.Find(i => i.itemType == itemType).soInt.value += amount;
+        }
+
+        public void RemoveByType(ItemType itemType, int amount = -1)
+        {
+            if (amount > 0) return;
+
+            var item = itensSetups.Find(i => i.itemType == itemType);
+            item.soInt.value -= amount;
+
+            if(item.soInt.value < 0) item.soInt.value = 0;
+        }
+
+        [NaughtyAttributes.Button]
+        private void AddCoin()
+        {
+            AddByType(ItemType.COIN);
+        }
         
+        
+        [NaughtyAttributes.Button]
+        private void AddLife()
+        {
+            AddByType(ItemType.LIFE_PACK);
+        }
+
     }
 
-    private void Start() {
-        Reset();    
-    }
-
-    private void Reset() {
-        AddCoins();
-        coins.value = 0;
-    }
-
-    public void AddCoins(int amount = 1) {
-        coins.value += amount;
-    }
-
+    [System.Serializable]
+    public class ItensSetup
+        {
+            public ItemType itemType;
+            public SOInt soInt;        
+        }
 }
