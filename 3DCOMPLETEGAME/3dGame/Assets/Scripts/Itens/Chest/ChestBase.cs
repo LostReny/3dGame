5e3bd570bbcff75fdea610c60d5cbfab125b331e
Isabelle Reny <isabelleribaldo@hotmail.com>
2025-01-05 22:12:28 -0300

    Chest Item Coins

--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Chest/ChestBase.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Chest/ChestBase.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class ChestBase : MonoBehaviour
 {
     public Animator animator;
+
+    [Header("Key")]
+    public KeyCode _keycode = KeyCode.Z;
 
     [Header("Trigger")]
     public string triggerOpen = "Open";
@@ -16,28 +20,63 @@ public class ChestBase : MonoBehaviour
     public GameObject _notification;
     public float tweenDuration = .2f;
     public Ease tweenEase = Ease.OutBack;
+
+    [Header("Text")]
+    public TMP_Text ChestText;
+    public bool usedItemOnce = false;
+
+    [Space]
+    public ChestItemBase chestItemBase;
+
+    
     private float startScale;
+
+    private bool _chestOpened = false;
+    private bool _playerNear = false;
+
 
     public void Start()
     {
         _chestCollider.enabled = true;
         _notification.SetActive(false);
         startScale = _notification.transform.localScale.x;
+        usedItemOnce = false;
     }
 
     [NaughtyAttributes.Button]
     private void OpenChest()
     {
+        if (_chestOpened) return;
         animator.SetTrigger(triggerOpen);
         _chestCollider.enabled = false;
-        StartCoroutine(ShowNotification(0.5f));
+        _chestOpened = true;
+        Invoke(nameof(ShowItemInChest), .5f);
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            OpenChest();
+            StartCoroutine(ShowNotification(0.5f));
+            ChestText.text = "Aperte Z para interagir";
+            usedItemOnce = true;
+            _playerNear = true;
+            InputChest();
+            StartCoroutine(HideChestMessage(3f));
+        }
+    }
+
+    private IEnumerator HideChestMessage(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ChestText.text = "";
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            _playerNear = false;
         }
     }
 
@@ -55,4 +94,29 @@ public class ChestBase : MonoBehaviour
         yield return new WaitForSeconds(delay);
         _notification.SetActive(false);
     }
+
+    private void Update()
+    {
+        InputChest();
+    }
+
+    private void InputChest()
+    {
+        if (Input.GetKeyDown(_keycode) && _playerNear && !_chestOpened)
+        {
+            OpenChest();
+        }
+    }
+
+    private void ShowItemInChest()
+    {
+        chestItemBase.ShowItem();
+        Invoke(nameof(CollecItemInChest), 0.8f);
+    }
+
+    private void CollecItemInChest()
+    {
+        chestItemBase.Collect();
+    }
+
 }
---
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/Chest/ChestItemCoin.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Itens;
+
+public class ChestItemCoin : ChestItemBase
+{
+    public int coinNumber = 5;
+    public GameObject coinObject;
+
+    private List<GameObject> _items = new List<GameObject>();
+
+    public Vector2 randomRange = new Vector2(-2f, 2f);
+
+    public float tweenEndTime = .5f;
+
+    public override void ShowItem()
+    {
+        base.ShowItem();
+        CreateItems();
+    }
+
+    [NaughtyAttributes.Button]
+    private void CreateItems()
+    {
+        for (int i = 0; i < coinNumber; i++) 
+        {
+            var item = Instantiate(coinObject);
+            item.transform.position = transform.position + Vector3.forward * Random.Range(randomRange.x, randomRange.y) + Vector3.right * Random.Range(randomRange.x, randomRange.y);
+            item.transform.DOScale(0, .2f).SetEase(Ease.OutBack).From();
+            item.transform.DORotate(new Vector3(0, 360, 0), 2f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+            _items.Add(item);
+        }
+    }
+
+    [NaughtyAttributes.Button]
+    public override void Collect()
+    {
+        base.Collect();
+        foreach(var i in _items)
+        {
+            i.transform.DOKill();
+            i.transform.DOMoveY(2f, tweenEndTime).SetRelative();
+            i.transform.DOScale(0, tweenEndTime / 2 ).SetDelay(tweenEndTime / 2);
+            ItemCollectManager.Instance.AddByType(ItemType.COIN);
+        }
+    }
+
+}
