cddff15aa13064680581d530ce77eb97196b6399
Isabelle Reny <68451719+LostReny@users.noreply.github.com>
2025-01-06 17:48:22 -0300

    Coins Drop
    Item destroy after used
    Limit of coins drop

--- 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/DestructableItem/DestructableItemBase.cs
+++ 3DCOMPLETEGAME/3dGame/Assets/Scripts/Itens/DestructableItem/DestructableItemBase.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Enemy;
 
 public class DestructableItemBase : MonoBehaviour
 {
@@ -9,6 +10,15 @@ public class DestructableItemBase : MonoBehaviour
 
     public float shakeDuration = .1f;
     public int shakeForce = 1;
+
+    public MeshRenderer meshRenderer;
+
+    [Header("Coins")]
+    public int dropCoinsAmount = 10;
+    public GameObject coinPrefab;
+    public Transform posCoin;
+    public int LimitOfDrop;
+    public bool canDrop = true;
 
     private void OnValidate()
     {
@@ -24,9 +34,65 @@ public class DestructableItemBase : MonoBehaviour
         }
     }
 
+    public void Start()
+    {   
+        canDrop = LimitOfDrop > 0;
+        TurnItOn();
+    }
+
     private void OnDamage(HealthBase h)
     {
         Debug.Log("On Damage");
         transform.DOShakeScale(shakeDuration, Vector3.up / 4, shakeForce);
+        DropCoins();
     }
+
+    #region COIN
+
+    [NaughtyAttributes.Button]
+    public void DropCoins()
+    {
+        if(!canDrop) return;
+
+        for (int i = 0; i < dropCoinsAmount; i++)
+        {
+            var coin = Instantiate(coinPrefab, posCoin.position, Quaternion.identity);
+            
+            Rigidbody rb = coin.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                Vector3 randomForce = new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(0.1f, 0.2f), Random.Range(-0.2f, 0.2f));
+                rb.AddForce(randomForce, ForceMode.Impulse);
+            }
+            coin.transform.DOScale(0, .5f).SetEase(Ease.OutBack).From();
+        }
+
+        LimitOfCoinsDrop();
+
+    }
+
+
+    public void LimitOfCoinsDrop()
+    {
+        LimitOfDrop--;
+        if (LimitOfDrop <= 0)
+        {
+            canDrop = false;
+            TurnItOff();
+        }
+    }
+    #endregion  
+
+    #region MESH FEEDBACK
+    private void TurnItOn()
+   {
+        meshRenderer.material.SetFloat("_Metallic", 0.902f);
+   } 
+    private void TurnItOff()
+   {
+        meshRenderer.material.SetFloat("_Metallic", 0.0f);
+   } 
+
+
+    #endregion
 }
